1f907925d95d6e7551ab30325ac41eb63687e270
Xarpan-cyber <arpanmahapatra06@gmail.com>
2026-09-07 18:13:41 +0530

    Add Express server and MongoDB

--- server.js
+++ server.js
@@ -72,6 +72,10 @@ app.delete('/api/transactions', requireUserId, async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
